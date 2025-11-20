2144ab98e7052876f56d8dc2226fe3907d79dc5c
boschkalex-cmd <boschk.alex@gmail.com>
2025-11-20 20:03:36 +0300

    created mainloop

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello world!')
+from pygame import *
+
+window = display.set_mode((900, 800))
+display.set_caption('Ping-Pong')
+
+game = True
+timer = time.Clock()
+
+while game:
+    window.fill((200, 130, 255))
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+    timer.tick(80)
