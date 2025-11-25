93b8dfebcc029b99b5d051691ed7a9b32f26626f
boschkalex-cmd <boschk.alex@gmail.com>
2025-11-25 19:29:21 +0300

    aded GameSprite class

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ display.set_caption('Ping-Pong')
 game = True
 timer = time.Clock()
 
+class GameSprite(sprite.Sprite):
+    def init(self, filename, x, y, width=30, height=200):
+        super.__init__()
+        self.image = transform.scale(image.load(filename), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 while game:
     window.fill((200, 130, 255))
     for e in event.get():
